9ee51365024b7b9bac80775ac2ef1422b0b09902
Marijn Jansen <therijn@users.noreply.github.com>
2017-06-19 16:02:48 +0200

    Fragments change working!

--- app/MyHyvesBookPlusStagram/app/src/main/java/nl/myhyvesbookplus/tagram/MainActivity.java
+++ app/MyHyvesBookPlusStagram/app/src/main/java/nl/myhyvesbookplus/tagram/MainActivity.java
@@ -1,35 +1,55 @@
 package nl.myhyvesbookplus.tagram;
 
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity implements CameraFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements CameraFragment.OnFragmentInteractionListener, ProfileFragment.OnFragmentInteractionListener, TimelineFragment.OnFragmentInteractionListener {
     final static private String TAG = "MainScreen";
 
     FirebaseAuth mAuth;
-    CameraFragment cameraFragment;
-
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
             switch (item.getItemId()) {
                 case nl.myhyvesbookplus.tagram.R.id.navigation_timeline:
+                    Log.d(TAG, "onNavigationItemSelected: Timeline");
+                    TimelineFragment timeline = new TimelineFragment();
+                    transaction.replace(R.id.content, timeline);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
                     return true;
+
                 case nl.myhyvesbookplus.tagram.R.id.navigation_camera:
+                    Log.d(TAG, "onNavigationItemSelected: Camera");
+                    CameraFragment camera = new CameraFragment();
+                    transaction.replace(R.id.content, camera);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
                     return true;
+
                 case nl.myhyvesbookplus.tagram.R.id.navigation_profile:
+                    Log.d(TAG, "onNavigationItemSelected: Profile");
+                    ProfileFragment profile = new ProfileFragment();
+                    transaction.replace(R.id.content, profile);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
                     return true;
             }
             return false;
@@ -45,19 +65,21 @@ public class MainActivity extends AppCompatActivity implements CameraFragment.On
         BottomNavigationView navigation = (BottomNavigationView) findViewById(nl.myhyvesbookplus.tagram.R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        FragmentManager fragmentManager = getFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
         mAuth = FirebaseAuth.getInstance();
         if (mAuth.getCurrentUser() == null) {
             goToLogin();
         }
 
+        TimelineFragment fragment = new TimelineFragment();
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, fragment);
+        transaction.commit();
+
     }
 
     @Override
     public void onBackPressed() {
-//        super.onBackPressed();
         finish();
     }
 
